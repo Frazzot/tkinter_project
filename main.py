9ce334a7dce4abc2e9edacbd92a05550db371f84
Ida Franzén Karlsson <ida.franzenkarlsson@itggot.se>
2018-05-09 17:11:21 +0200

    Resized window and added login layout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Bank, inventory, profile, sorting system, items
-from tkinter import Frame, Tk, Menu, BOTH
+from tkinter import Frame, Tk, Menu, BOTH, Entry, StringVar, Button
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -10,6 +10,7 @@ class Window(Frame):
     def init_window(self):
         self.master.title("Tkinter Project")
         self.pack(fill=BOTH, expand=1)
+        # self.grid(row=0, column=0, sticky="nsew")
 
         menu = Menu(self.master)
         self.master.config(menu=menu)
@@ -18,15 +19,36 @@ class Window(Frame):
         users.add_command(label="Bank")
         users.add_command(label="Inventory")
         users.add_command(label="Profile")
+        users.add_command(label="Create account")
         menu.add_cascade(label="Users", menu=users)
 
         store = Menu(menu, tearoff=0)
         store.add_command(label="Sell")
         store.add_command(label="Purchase")
         menu.add_cascade(label="Store", menu=store)
+        username_holder = StringVar()
+        password_holder = StringVar()
+        b = Button(self, text="Login", command=self.callback)
+        e1 = Entry(self, textvariable=username_holder)
+        e2 = Entry(self,textvariable=password_holder)
+        username_holder.set("Username")
+        password_holder.set("Password")
+        e1.pack()
+        e2.pack()
+        b.pack()
 
-
+    
+    def callback(self):
+        print("click!")
+    
 root = Tk()
-root.geometry("1000x600")
+
+
+
+
+
+root.geometry("600x500")
 app = Window(root)
 root.mainloop()
+
+#TODO: Add entrys and stringvars plus login button
