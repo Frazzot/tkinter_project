2ffdfa068c4c85114e06ba0559d2c65e8efac91f
Ida Franzén Karlsson <ida.franzenkarlsson@itggot.se>
2018-05-10 20:42:38 +0200

    Added new class for ca_window and function for show_frames

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Bank, inventory, profile, sorting system, items
 from tkinter import Frame, Tk, Menu, BOTH, Entry, StringVar, Button
 
-class Window(Frame):
+class MyMenu(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.__init__ = master
-        self.init_window()
+        self.menu()
+        self.frames = [Login_window(self), Ca_window(self)]
 
-    def init_window(self):
+    def menu(self):
         self.master.title("Tkinter Project")
         self.pack(fill=BOTH, expand=1)
-        # self.grid(row=0, column=0, sticky="nsew")
 
         menu = Menu(self.master)
         self.master.config(menu=menu)
@@ -19,13 +19,29 @@ class Window(Frame):
         users.add_command(label="Bank")
         users.add_command(label="Inventory")
         users.add_command(label="Profile")
-        users.add_command(label="Create account")
+        users.add_command(label="Create account", command=lambda: self.show_frame(1))
         menu.add_cascade(label="Users", menu=users)
 
         store = Menu(menu, tearoff=0)
         store.add_command(label="Sell")
         store.add_command(label="Purchase")
         menu.add_cascade(label="Store", menu=store)
+    
+    def show_frame(self, i):
+        for x in range(len(self.frames)):
+            if x != i:
+                self.frames[x].pack_forget()
+        self.frames[i].pack()
+
+
+class Login_window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.init_window()
+
+    def init_window(self):
+        #self.master.title("Tkinter Project")
+        self.pack(fill=BOTH, expand=1)
         username_holder = StringVar()
         password_holder = StringVar()
         b = Button(self, text="Login", command=self.callback)
@@ -36,20 +52,36 @@ class Window(Frame):
         e1.pack()
         e2.pack()
         b.pack()
-
-    
+  
     def callback(self):
         print("click!")
     
-class Window_2(Frame):
+class Ca_window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.__init__ = master
+        self.init_ca_window()
+
+    def init_ca_window(self, master=None):
+        # self.master.title("Tkinter Project")
+        self.pack(fill=BOTH, expand=1)
+        username_holder = StringVar()
+        password_holder = StringVar()
+        b = Button(self, text="Create Account")
+        e1 = Entry(self, textvariable=username_holder)
+        e2 = Entry(self,textvariable=password_holder)
+        username_holder.set("Username")
+        password_holder.set("Password")
+        e1.pack()
+        e2.pack()
+        b.pack()
+
+
 root = Tk()
-
-
-
-
-
+root.title("Tkinter project")
 root.geometry("600x500")
-app = Window(root)
+app = MyMenu(root)
+app.show_frame(0)
 root.mainloop()
 
-#TODO: Add entrys and stringvars plus login button
+#TODO: New frame class for the menu
